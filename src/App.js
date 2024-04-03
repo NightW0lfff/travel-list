2ce1065935f3b3ab9c05db6ce7e14389d317ae8c
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-04-03 15:41:40 +1100

    Finished layout and display the webpage with functional elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { initialItems } from "./data";
+import { useState } from "react";
+
+let initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 
 export default function App() {
   return (
@@ -16,17 +22,38 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, package: false, id: Date.now() };
+
+    setDescription("");
+    setQuantity(1);
+
+    console.log(newItem);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3 className="">What do you need for your trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item ..." />
+      <input
+        type="text"
+        placeholder="Item ..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
-export const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+export function Data() {
+  let initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Charger", quantity: 1, packed: true },
+  ];
+
+  function addItem(item) {
+    initialItems.push(item);
+  }
+
+  return {
+    item: initialItems,
+    addItem: addItem,
+  };
+}
